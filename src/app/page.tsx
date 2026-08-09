a9aacfd5c3faa7b3dd962f5a9cf36692097f12de
Nicholas W <nicholaslwallace@gmail.com>
2026-08-09 01:02:27 -0700

    Update service page: organist, chorister, hymns, and special musical number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Page() {
       <div id="services" className="mx-2">
         <LineItemLeadership left="Presiding" right="Bishop Shumway" />
         <LineItemLeadership left="Conducting" right="Will Shoemaker" />
-        <LineItemLeadership left="Organist" right="Chris Cooley" />
-        <LineItemLeadership left="Chorister" right="Lisa Brandt" />
+        <LineItemLeadership left="Organist" right="Melody Williams" />
+        <LineItemLeadership left="Chorister" right="Laurel Cronin" />
 
         <div className="p-8 my-4 rounded bg-sky-100">
           <div className="text-center">
@@ -20,10 +20,10 @@ export default function Page() {
           </div>
           <ol className="list-decimal ml-5 sticky">
             <li className="pt-1">
-              Mission Plan, August Operative Practice - "Arrange to have the missionaries teach a lesson on member missionary work during: Family Home Evenings, Missionary Visits to Members, and/or At Missionary Meal Times."
+              Mission Plan, August Operative Practice - "Arrange to have the missionaries teach a lesson on member missionary work during: Family Home Evenings, Missionary Visits to Members, and/o[...]
             </li>
             <li className="pt-1">
-              Mesa250 9/11 commemoration on September 12. Donations of books and clothing (infant through adult) are being collected. There are collection boxes at the Cornia, Speakman, and Hakes homes.
+              Mesa250 9/11 commemoration on September 12. Donations of books and clothing (infant through adult) are being collected. There are collection boxes at the Cornia, Speakman, and Hakes [...]
             </li>
             <li className="pt-1">
               The Ward Pool Party is on August 29 from 4-6 PM at the Brimhall Aquatic Center. Bring a dessert or snack to share.
@@ -54,44 +54,35 @@ export default function Page() {
 
         <LineItem
           left="Opening Hymn"
-          middle="Come, O Thou King of Kings"
-          right="59"
-          link="https://www.churchofjesuschrist.org/media/music/songs/come-o-thou-king-of-kings?lang=eng"
+          middle="The Spirit of God"
+          right="2"
+          link="https://www.churchofjesuschrist.org/media/music/songs/the-spirit-of-god?lang=eng"
         />
         <LineItem left="Invocation" right="By Invitation" />
 
         <LineItemBold middle="Ward Business" />
 
         <LineItem
-          left="Sacrament Hymn"
-          middle="How Great the Wisdom and the Love"
-          right="195"
-          link="https://www.churchofjesuschrist.org/media/music/songs/how-great-the-wisdom-and-the-love?lang=eng"
+          left="Sacrament"
+          middle="Jesus, Once of Humble Birth"
+          right="196"
+          link="https://www.churchofjesuschrist.org/media/music/songs/jesus-once-of-humble-birth?lang=eng"
         />
 
         <LineItemBold middle="Administration of the Sacrament" />
 
         <LineItemBold middle="Bearing of Testimonies" />
 
-        {/*
-        <LineItem left="Speaker" right="Trevor Hardy" />
-        <LineItem left="Speaker" right="Scott Wininger" />
-
         <LineItem
           left="Special Musical Number"
-          middle="Praise to the Man"
-          right="27"
-          link="https://www.churchofjesuschrist.org/media/music/songs/praise-to-the-man?lang=eng"
+          middle="My Heavenly Father Loves Me — Jayna's friends"
         />
-
-        <LineItem left="Speaker" right="Joe Capps" />
-        */}
 
         <LineItem
           left="Closing Hymn"
-          middle="Come Follow Me"
-          right="116"
-          link="https://www.churchofjesuschrist.org/media/music/songs/come-follow-me?lang=eng"
+          middle="I Need Thee Every Hour"
+          right="98"
+          link="https://www.churchofjesuschrist.org/media/music/songs/i-need-thee-every-hour?lang=eng"
         />
 
         <LineItem left="Benediction" right="By Invitation" />
